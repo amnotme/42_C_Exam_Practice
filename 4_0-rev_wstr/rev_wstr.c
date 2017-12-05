42d2011a324d9a52cfbc6d48f02f85c315118525
amnotme <amnotme@icloud.com>
2017-12-05 01:39:54 -0800

    added rev_wstr.c

--- 4_0-rev_wstr/rev_wstr.c
+++ 4_0-rev_wstr/rev_wstr.c
@@ -1,62 +1,95 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rev_wstr.c                                         :+:      :+:    :+:   */
+/*   2rev_wstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhernand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: leopoldohernandez <marvin@42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/02 22:34:32 by lhernand          #+#    #+#             */
-/*   Updated: 2017/12/02 22:39:09 by lhernand         ###   ########.fr       */
+/*   Created: 2017/12/04 17:52:35 by leopoldoh         #+#    #+#             */
+/*   Updated: 2017/12/05 01:21:58 by leopoldoh        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void        ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
-void		ft_putstr(char *str)
-{
-	int i;
 
-	i = 0;
-	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
+void        ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+int         ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
 }
 
-int			wsapce(char c)
+int         wspace(char c)
 {
-	if (c == ' ' || c == '\t')
-		return (1);
-	return (0);
+    if (c == ' ' || c == '\t')
+        return (1);
+    return (0);
 }
 
-void		rev_wstr(char *arg)
+void        rev_wstr(char *arg)
 {
-	int i; 
-	int j;
+    int l;
+    int i;
+    int j;
 
-	i = 0;
-	j = 0;
-	while (arg[i])
-	{
-		TODO
-	}
+    i = 0;
+    l = ft_strlen(arg) - 1;
+    while (l >= 0)
+    {
+        if (!wspace(arg[l]))
+            i++;
+        if (wspace(arg[l]) || l == 0)
+        {
+            if (l == 0)
+                j = l;
+            else
+            {
+                l++;
+                j = l;
+            }
+            while (i > 0)
+            {   
+
+                ft_putchar(arg[j]);
+                i--;
+                j++;
+            }
+            l--;
+            if (l >= 0)
+                ft_putchar(' ');
+        }
+        l--;
+    }
 }
 
-int			main(int argc, char **argv)
+int         main(int argc, char **argv)
 {
-	if (argc != 2)
-		ft_putchar('\n');
-	else
-	{
-		rev_wstr(argv[1]);
-		ft_putchar('\n');
-	}
-	return (0);
+    if (argc != 2)
+        ft_putchar('\n');
+    else
+    {
+        rev_wstr(argv[1]);
+        ft_putchar('\n');
+    }
+    return (0);
 }
